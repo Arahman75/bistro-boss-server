c0a1e14b27f3c47ec8bf96adbc5938267b7807cc
Arahman75 <selfarahman01825@gmail.com>
2024-01-10 21:25:52 +0600

    added the user api

--- index.js
+++ index.js
@@ -28,10 +28,46 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const userCollection = client.db("bistroDb").collection('users');
         const menuCollection = client.db("bistroDb").collection('menu');
         const reviewCollection = client.db("bistroDb").collection('reviews');
         const cartCollection = client.db("bistroDb").collection('carts');
 
+        // User related api
+        // app.post('/users', async (req, res) => {
+        //     const user = req.body;
+        //     const result = await userCollection.insertOne(user);
+        //     res.send(result);
+        // })
+        // social email id soho client teke database a add kora
+        // app.post('/users', async (req, res) => {
+        //     const user = req.body;
+        //     // insert email if does not exists
+        //     // you can do this many ways(1. email unique 2. upsert 3. simple checking)
+        //     const query = { email: user.email };
+        //     const existingUser = await userCollection.findOne(query);
+        //     if (existingUser) {
+        //         return res.send({ message: 'user already exists', insertedId: null })
+        //     }
+        //     const result = await userCollection.insertOne(user);
+        //     res.send(result);
+        // })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            // insert email if user doesnt exists: 
+            // you can do this many ways (1. email unique, 2. upsert 3. simple checking)
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exists', insertedId: null })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
+
+
+        // menu related api
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
             res.send(result)
